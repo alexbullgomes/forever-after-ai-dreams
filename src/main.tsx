dd5a37b72d195b1d1edfe887d96fde9167e1ed27
fernandesyuri33 <yurifoliveira33@gmail.com>
2025-10-29 20:00:59 +0100

    load css on html

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById("root")!).render(
